567bff6337f8ea2702978bb22dc02e2aebfffadc
mauro <maurodelle7@gmail.com>
2022-06-17 22:47:39 -0300

    Add files via upload
    Subida TP_[4]

---
+++ tp_laboratorio_1/TP_[4]/Debug/testing/subdir.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../testing/utest.c 
+
+C_DEPS += \
+./testing/utest.d 
+
+OBJS += \
+./testing/utest.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+testing/%.o: ../testing/%.c testing/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-testing
+
+clean-testing:
+	-$(RM) ./testing/utest.d ./testing/utest.o
+
+.PHONY: clean-testing
+
