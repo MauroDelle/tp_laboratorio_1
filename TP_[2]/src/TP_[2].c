3272759918f91ca936b58c87fe2ff311e4988048
MauroDelle <maurodelle7@gmail.com>
2022-10-13 15:36:09 -0300

    Iniciando TP_[2]

---
+++ TP_[2]/src/TP_[2].c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : TP_[2].c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	setbuf(stdout,NULL);
+
+
+	printf("Hola");
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
